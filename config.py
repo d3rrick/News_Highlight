20a6cd9b5520e3048a1295f6e32979cfc306ed99
muriithiderro <muriithiderrick56@gmail.com>
2018-05-14 06:08:31 +0300

    added a url base for the sources

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
-import os
+import os #this is an inbult method that allows access to the operating system variables like the environment and directories
 
 class Config:
+	"""this is the base config class that sets out some variables"""
 	NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country={}&category={}&apiKey={}'
 	NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
 	NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
 	SECRET_KEY = os.environ.get("SECRET_KEY")
 
 class ProdConfig(Config):
-	pass
+	"""this configuration class is for setting production variables"""
+
+	DEBUG = False
 
 class DevConfig(Config):
+	"""this configuration class is for setting development variables"""
 	DEBUG = True
 
 
