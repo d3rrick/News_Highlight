3aed354d688d30f931b01b59ae67021c1e7a8568
muriithiderro <muriithiderrick56@gmail.com>
2018-05-14 06:35:03 +0300

    added tests to manage.py file

--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
 from app import create_app
 
 from flask_script import Manager, Server
+from . import tests
 
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+@manager.command
+tests
+
 
 if __name__ == '__main__':
 	"""this code executes if this file is run as the main file"""
