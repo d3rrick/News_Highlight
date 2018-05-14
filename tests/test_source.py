7d5d85ebba19df603c5a6de026f9a9d95e7d41d8
muriithiderro <muriithiderrick56@gmail.com>
2018-05-14 10:21:07 +0300

    added methods for testing instances

--- tests/test_source.py
+++ tests/test_source.py
@@ -2,19 +2,19 @@ import unittest
 from app.models import Source
 
 class SourceTest(unittest.TestCase):
-	 '''
-    Test Class to test the behaviour of the Source class
-    '''
+	'''
+	Test Class to test the behaviour of the Source class
+	'''
 	def setUp(self):
-		 '''
-        Set up method that will run before every Test
-        '''
+		'''
+		Set up method that will run before every Test
+		'''
 		self.new_source = Source("unique id","new times","this article is about how to spend your mornings efficiently","https://google.com","health","us","en")
 
 	def test_instance(self):
-		 '''
-        A method that will run to test whether an instance is of Article class.
-        '''
+		'''
+		A method that will run to test whether an instance is of Article class.
+		'''
 		self.assertTrue(isinstance(self.new_source,Source))
 	def test_correct_instatiation(self):
 		"""
@@ -23,5 +23,3 @@ class SourceTest(unittest.TestCase):
 		self.assertEqual(self.new_source.id,"unique id")
 
 		
-if __name__ == '__main__':
-	unittest()
