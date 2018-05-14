f352cc1b43c5a1802d90419da417a8d7bf0440a6
muriithiderro <muriithiderrick56@gmail.com>
2018-05-14 06:35:38 +0300

    added test classes and methods to the Article class

--- tests/test_article.py
+++ tests/test_article.py
@@ -1,5 +1,27 @@
+import unittest
 from app.models import Article
 
-print(Article)
-# if __name__ == '__main__':
-# 	main()
+class ArticleTest(unittest.TestCase):
+	 '''
+    Test Class to test the behaviour of the Article class
+    '''
+	def setUp(self):
+		 '''
+        Set up method that will run before every Test
+        '''
+		self.new_article = Article(1,"new times",'NEW_TIMES',"the cool morning","this article is about how to spend your mornings efficiently","https://google.com","https://google.com/image.jpg",True)
+
+	def test_instance(self):
+		 '''
+        A method that will run to test whether an instance is of Article class.
+        '''
+		self.assertTrue(isinstance(self.new_article,Article))
+	def test_correct_instatiation(self):
+		"""
+		A method that allows testing whether instatiation is done properly
+		"""
+		self.assertEqual(self.new_article.id, 1)
+
+		
+if __name__ == '__main__':
+	unittest()
