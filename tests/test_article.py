235225598397bc20c64ff1c416b46287ae8891a2
muriithiderro <muriithiderrick56@gmail.com>
2018-05-14 10:20:59 +0300

    added methods for testing instances

--- tests/test_article.py
+++ tests/test_article.py
@@ -2,19 +2,19 @@ import unittest
 from app.models import Article
 
 class ArticleTest(unittest.TestCase):
-	 '''
-    Test Class to test the behaviour of the Article class
-    '''
+	'''
+	Test Class to test the behaviour of the Article class
+	'''
 	def setUp(self):
-		 '''
-        Set up method that will run before every Test
-        '''
+		'''
+		Set up method that will run before every Test
+		'''
 		self.new_article = Article(1,"new times",'NEW_TIMES',"the cool morning","this article is about how to spend your mornings efficiently","https://google.com","https://google.com/image.jpg",True)
 
 	def test_instance(self):
-		 '''
-        A method that will run to test whether an instance is of Article class.
-        '''
+		'''
+		A method that will run to test whether an instance is of Article class.
+		'''
 		self.assertTrue(isinstance(self.new_article,Article))
 	def test_correct_instatiation(self):
 		"""
@@ -22,6 +22,4 @@ class ArticleTest(unittest.TestCase):
 		"""
 		self.assertEqual(self.new_article.id, 1)
 
-		
-if __name__ == '__main__':
-	unittest()
+
